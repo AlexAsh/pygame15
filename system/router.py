816eb2b98c1bf05c35da6cdbf9212bc8f57b2e70
Ash <streamprop@gmail.com>
2015-03-03 20:36:57 +0400

    Basic router implementation

---
+++ system/router.py
@@ -0,0 +1,23 @@
+"""Routing events to controller's actions"""
+
+import pygame
+
+
+class Router:
+    """Resolves events to controller's actions, handling mapping table"""
+
+    def __init__(self):
+        self.mapping = {
+            pygame.QUIT:            ("GeneralController", "quit"),
+            pygame.MOUSEBUTTONDOWN: ("MouseController",   "down"),
+            pygame.MOUSEBUTTONUP:   ("MouseController",   "up"),
+            pygame.MOUSEMOTION:     ("MouseController",   "move"),
+            pygame.ACTIVEEVENT:     ("GeneralController", "toggle_focus")
+        }
+
+    def route(self, event):
+        """Get controller and action by event"""
+        if event.type not in self.mapping:
+            return None
+
+        return self.mapping[event.type]
