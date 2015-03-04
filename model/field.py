34f72f88ccf7cf3ea62c284163cd15f8fca5f72a
Ash <streamprop@gmail.com>
2015-03-04 15:04:11 +0400

    Ball and Field models implementation

--- model/field.py
+++ model/field.py
@@ -10,3 +10,15 @@ class Field:
     def get_size(self):
         """Get field size"""
         return self.size
+
+    def move_ball(self, ball):
+        ball.position[0] += ball.speed[0]
+        ball.position[1] += ball.speed[1]
+
+        if (ball.position[0] + ball.radius >= self.size[0] or
+                ball.position[0] - ball.radius <= 0.0):
+            ball.speed[0] = -ball.speed[0]
+
+        if (ball.position[1] + ball.radius >= self.size[1] or
+                ball.position[1] - ball.radius <= 0.0):
+            ball.speed[1] = -ball.speed[1]
